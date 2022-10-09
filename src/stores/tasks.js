037914ee6b34c926e11db3b4c45228be1bb0ade0
Meddy <kaluumamhd@gmail.com>
2022-10-09 20:16:03 +0300

    state and getters

--- src/stores/tasks.js
+++ src/stores/tasks.js
@@ -1,10 +1,26 @@
 import { defineStore } from 'pinia'
 
-export const useTaskStore = defineStore('tasks', () => {
+export const useTaskStore = defineStore('tasks', {
 	state: () => ({
 		tasks: [
-			{id: 1, title: 'Buy some milk', isFave: false},
-			{id: 2, title: 'Play Max Payne', isFave: true},
+			{id: 1, title: 'Buy some milk', isFav: false},
+			{id: 2, title: 'Play Max Payne', isFav: true},
 		]
-	})
+	}),
+
+	getters: {
+		favs() {
+			return this.tasks.filter(t => t.isFav)
+		},
+
+		favCount() {
+			return this.tasks.reduce((p, c) => {
+				return c.isFav ? p + 1 : p
+			}, 0)
+		},
+
+		totalCount: (state) => {
+			return state.tasks.length
+		}
+	}
 })
